ecfacdfc508ceec1b1fc72cb314bb1cc919f3800
Prunelle Moulaire <prunelle.moulaire@gmail.com>
2021-09-01 17:11:20 +0200

    mise à jour des fichiers api.py, front-streamlit.py et model.py avec code Emeline (wouhou) et creation Docker

--- api.py
+++ api.py
@@ -4,73 +4,84 @@ import pandas as pd
 import numpy as np
 import pickle
 from tensorflow.keras.applications.imagenet_utils import (decode_predictions)
-from model import load_model, predict, prepare_image
+from model import load_model, predict, prepare_image, get_prediction_pictures
 from pydantic import BaseModel
 from typing import List
-from io import BytesIO
+from io import BytesIO, StringIO
 from PIL import Image
 from fastapi import FastAPI, File, HTTPException, UploadFile
 import uvicorn
+import matplotlib.pyplot as plt
 
-# model = load_model()
+# bloc ci-dessous décomenté lors du fonctionnement de l'api alexandre
+model = load_model()
 
-# #Define the response JSON
-# class Prediction(BaseModel):
-#     filename: str
-#     content_type: str
-#     predictions: List[dict] = []
-# app = FastAPI()
+#Define the response JSON
+class Prediction(BaseModel):
+    filename: str
+    content_type: str
+    predictions: List[dict] = []
+
+app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Allows all origins
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
-    allow_headers=["*"],  # Allows all headers
-)
+    allow_headers=["*"])  # Allows all headers
+
 
 @app.get("/test-operation-bidon")
 def test_operation_bidon(entered_data):
     return (f"{int(entered_data)*2} millions de papillons")
 
-# @app.get("/predict-image")
-# def predict_image(url):
-#     pkl_file = open(url, 'rb')
-#     image = pickle.load(pkl_file)
-#     im = Image.open(url)
-#     im.show()
-#     pkl_file.close()
-#     return ("image ok")
+@app.get("/predict-image")
+def predict_image(url):
+    pkl_file = open(url, 'rb')
+    image = pickle.load(pkl_file)
+    im = plt.imread(image)
+    image = prepare_image(im)
+    prediction = predict(image, model)
+    pkl_file.close()
+    dico = {}
+    for i in prediction.keys():
+        nom_latin = i
+        pkl_files = get_prediction_pictures(i)
+        dico[prediction[i]] = (nom_latin, pkl_files)
+    return dico
+
 
 #code qui fonctionne (Alexandre)
-@app.post("/predict")
-async def prediction(file: UploadFile = File(...)):
-    # Initialize the data dictionnary that will be returned
-    print('before file check')
-    if not file.content_type.startswith("image/"):
-        raise HTTPException(status_code=400, detail="File provided is not an image.")
-    content = await file.read()
-    image = Image.open(BytesIO(content)).convert("RGB")
-    #Faire la prédiction en utilisant l'image
-    return "toto"
-
 # @app.post("/predict")
 # async def prediction(file: UploadFile = File(...)):
-#     # Initialize the data dictionnary that will be returned
 #     print('before file check')
 #     if not file.content_type.startswith("image/"):
 #         raise HTTPException(status_code=400, detail="File provided is not an image.")
 #     content = await file.read()
 #     image = Image.open(BytesIO(content)).convert("RGB")
-#     #Faire la prédiction en utilisant l'image
-#     image = prepare_image(image, target=(224, 224))
-#     response = predict(image, model)
-#     # return the response as a JSON
-#     return {
-#         "filename": file.filename,
-#         "content_type": file.content_type,
-#         "predictions": response,
-#     }
+#     return "toto"
+
+# @app.post("/predict")
+# async def prediction(file: UploadFile = File(...)):
+    # Initialize the data dictionnary that will be returned
+    # print('before file check')
+    # if not file.content_type.startswith("image/"):
+    #     raise HTTPException(status_code=400, detail="File provided is not an image.")
+    # img = Image.open(file.file).convert('RGB')
+    # make prediction
+    #image = Image.frombytes("RGB", (1000, 1000), content)
+    #image = Image.open(BytesIO(content)).convert("RGB")
+    #Faire la prédiction en utilisant l'image
+    # image = prepare_image(content, target=(224, 224))
+    # prediction = predict(image, model)
+    # return the response as a JSON
+    # return "toto"
+    # return {
+    #     "filename": file.filename,
+    #     "content_type": file.content_type,
+    #     "predictions": prediction,
+    # }
 
 # @app.post("/predict", response_model=Prediction)
 # async def prediction(file: UploadFile = File(...)):
--- front-streamlit.py
+++ front-streamlit.py
@@ -8,7 +8,9 @@ import time
 import requests
 import pickle
 import base64
-import io
+
+
+
 #CSS
 
 CSS = """
@@ -58,7 +60,8 @@ def background_image_style(path):
     </style>
     '''
     return style
-image_path = "/Users/prunelle/Downloads/9B2F199E-79BF-4EBD-B729-D518A58D1292_1_105_c.jpeg"
+#image_path = "/Users/prunelle/Downloads/9B2F199E-79BF-4EBD-B729-D518A58D1292_1_105_c.jpeg"
+image_path = "/Users/prunelle/Downloads/9eb59fed3bfe7fb6ad4cfbb9aaad2b7e.jpg"
 image_link = 'https://docs.python.org/3/'
 st.write(background_image_style(image_path), unsafe_allow_html=True)
 
@@ -68,73 +71,99 @@ st.set_option('deprecation.showfileUploaderEncoding', False)
 uploaded_file = st.file_uploader("", type=['png','jpeg','jpg'])
 
 if uploaded_file is not None:
-    file_details = {"FileName":uploaded_file.name,"FileType":uploaded_file.type,"FileSize":uploaded_file.size}
-    st.write(file_details)
+    #file_details = {"FileName":uploaded_file.name,"FileType":uploaded_file.type,"FileSize":uploaded_file.size}
+    #st.write(file_details)
     image = Image.open(uploaded_file)
-    st.image(image)
-    st.warning('Photo chargée avec succès !')
-    # utiliser request pour envoyer un fichier en method post
+    col1, col2 = st.columns(2)
+    with col1:
+        st.image(image)
+    with col2:
+        st.warning('Photo chargée avec succès !')
+        st.markdown("""#### *Vous pouvez lancer une nouvelle prédiction en glissant une nouvelle photo ou bien en fermant avec la croix*
+        """)
+    st.markdown("""## ... Lancement de l'algorithme Snapillon ... """)
+    latest_iteration = st.empty()
+    bar = st.progress(0)
+
+    for i in range(100):
+        # Update the progress bar with each iteration.
+        latest_iteration.text(f'Iteration {i+1}')
+        bar.progress(i + 1)
+        time.sleep(0.02)
+
+    st.markdown("""# Par ici les résultats !
+    """)
+    #PICKLE CONVERTION
+    url_image = "raw_data/Docker/test_image_pickle.pkl"
+    file = open(url_image, 'wb')
+    # Pickle dictionary using protocol 0.
+    pickle.dump(uploaded_file, file)
+    file.close()
+
+    parameters2 = dict(url = url_image)
+    url2 = 'http://127.0.0.1:8000/predict-image'
+    dico = requests.get(url2, params = parameters2).json()
+
+    for j, i in enumerate(dico.keys()):
+        st.markdown(f"""## Estimation n°{j+1} : Votre papillon est un *{dico[i][0].replace('_', " ")}*""") #sort le nom de l'espèce en latin
+        st.markdown(f"""### *Probabilité de succès : {round(float(i),3)}*""")
+        #st.markdown("")
+        pkl_file = open(dico[i][1], 'rb')
+        images = pickle.load(pkl_file)
+        pkl_file.close()
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.image(images[0])
+            pass
+        with col2:
+            st.image(images[1])
+        with col3 :
+            st.image(images[2])
+
+
+
 
 #barre d'avancement lors du calcul de l'algorithme
-if uploaded_file is not None:
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        pass
-    with col2:
-        center_button = st.button('Lancement algorithme Snapillon')
-    with col3 :
-        pass
-    if center_button:
-        latest_iteration = st.empty()
-        bar = st.progress(0)
-
-        for i in range(100):
-            # Update the progress bar with each iteration.
-            latest_iteration.text(f'Iteration {i+1}')
-            bar.progress(i + 1)
-            time.sleep(0.04)
-
-        st.markdown("""# Par ici les résultats !
-        """)
+# if uploaded_file is not None:
+#     col1, col2, col3 = st.columns(3)
+#     with col1:
+#         pass
+#     with col2:
+#         center_button = st.button('Lancement algorithme Snapillon')
+#     with col3 :
+#         pass
 
 
-#PICKLE CONVERTION
-# st.markdown("""# Convertir au format pickle""")
-# url_image = "/Users/prunelle/Downloads/test_image_pickel.pkl"
-# file = open(url_image, 'wb')
-# # Pickle dictionary using protocol 0.
-# pickle.dump(uploaded_file, file)
-# file.close()
 
-check1= st.checkbox('launch api multiplication papillons')
+#check1= st.checkbox('launch api multiplication papillons')
 #check2 = st.checkbox('launch api test image')
-check3 = st.checkbox('launch api test image 2')
+#check3 = st.checkbox('launch api test image 2')
 
 #retour API test connexion api : calcul multiplication papillons avec nb en entrée streamlit
-if check1:
-    st.markdown("""# API TEST MULTIPLICATION PAPILLONS""")
-    entered_data = int(st.slider('Entrer le nombre de papillons', 1, 6, 1))
-    url1 = 'http://127.0.0.1:8000/test-operation-bidon'
-    parameters = dict(entered_data = entered_data)
-    st.markdown(requests.get(url1, params = parameters).json())
+# if check1:
+#     st.markdown("""# API TEST MULTIPLICATION PAPILLONS""")
+#     entered_data = int(st.slider('Entrer le nombre de papillons', 1, 6, 1))
+#     url1 = 'http://127.0.0.1:8000/test-operation-bidon'
+#     parameters = dict(entered_data = entered_data)
+#     st.markdown(requests.get(url1, params = parameters).json())
 
-#retour API test image en entrée renvoie 'image ok'
-# if check2:
-#     st.markdown("""# API TEST IMAGE""")
-#     parameters2 = dict(url = url_image)
-#     url2 = 'http://127.0.0.1:8000/predict-image'
-#     st.markdown(requests.get(url2, params = parameters2).json())
+
+
+
+#slider_element.slider("Slide me!", 0, 100, key=session.run_id)
 
 #code qui fonctionne (Alexandre)
-if check3:
-    st.markdown("""# API TEST IMAGE 2""")
-    #parameters3 = dict(file = uploaded_file)
-    url2 = 'http://127.0.0.1:8000/predict'
-    response = requests.post(
-        url2, files={"file": ("media", io.BytesIO(uploaded_file.read()), "image/jpeg")}
-    )
-    assert response.status_code == 200
-    st.warning('Photo envoyée avec succès !')
+# if check3:
+#     st.markdown("""# API TEST IMAGE 2""")
+#     #parameters3 = dict(file = uploaded_file)
+#     url2 = 'http://127.0.0.1:8000/predict'
+#     print(type(uploaded_file))
+#     response = requests.post(
+#         url2, files={"file": ("media", uploaded_file, "image/jpeg")}
+#     )
+#     #assert response.status_code == 200
+#     st.warning('Photo envoyée avec succès !')
+#     st.markdown(response)
     #st.markdown(requests.post(url2, params = parameters3).json())
 
 # if check3:
@@ -170,7 +199,8 @@ if check3:
 #     sampling = data_to_sample.sample(nb_image)
 #     return sampling
 
-# #retourner les trois images et le nom de l'espèce
+#retourner les trois images et le nom de l'espèce
+
 # species = ['Parnassius apollo']
 # for i in species :
 #     st.markdown(f"""# Espèce identifiée : {i}""")
